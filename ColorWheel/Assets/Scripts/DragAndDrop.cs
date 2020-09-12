26cc7d98adb6d6a995679fa87bbb96f6a3999d67
MagicDoogies <37319450+MagicDoogies@users.noreply.github.com>
2020-09-12 15:13:58 -0400

    Added green and purple sprite to game./ Click and drag functionality/ sprite swap when needlte is attached.

---
+++ ColorWheel/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;//containter to grab the image size of the affected object.
+
+    [SerializeField] private Canvas canvas;//containter for UI canvas gameObject;
+    
+    
+    
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();//when the script is active take the dimensions of the image.
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;//First the script takes position of the cursor and ties the image position based on that. Next it divides the scale of the canvas so the image is correctly centered.
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        throw new System.NotImplementedException();
+    }
+    
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        throw new System.NotImplementedException();
+    }
+  public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+        throw new System.NotImplementedException();
+    }
+
+
+}
---
+++ ColorWheel/Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Sprite Green;
+    public Sprite Purple;
+    public Sprite Blue;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("You activated me");
+       if(collision.gameObject.tag == "YellowNeedle")//if the object interacting with blue creature is a yellow needle.
+        {
+            print(" You have the yellow needle");
+            this.gameObject.GetComponent<Image>().sprite = Green;
+        }
+        
+       if(collision.gameObject.tag == "RedNeedle")//if the object interacting with blue creature is a red needle.
+        {
+            print("You have the red needle");
+            this.gameObject.GetComponent<Image>().sprite = Purple;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)// When player moves the needles out of the trigger it reverts to it's original color.
+    {
+        this.gameObject.GetComponent<Image>().sprite = Blue;
+    }
+}
